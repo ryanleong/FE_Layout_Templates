e383d806a3eff961e67d43d8aac419e4dfc2eebf
Ryan Leong <iam@ryanleong.me>
2016-02-26 23:27:44 +0800

    - Added function to fadein and fadeout in layout framework.

--- Full Width Hero/app-src/js/_layout-framework.js
+++ Full Width Hero/app-src/js/_layout-framework.js
@@ -28,30 +28,3 @@ function fadeOut(element) {
         $(element).removeClass('show');
     });
 }
-
-;//@prepros-prepend _layout-framework.js
-
-$(document).ready(function() {
-
-    var openCallbackFn = function() {
-        $('#main.lightbox .wrapper').css({ "height": "200px", "width": "200", "background-color": "#fff"});
-    };
-    var closeCallbackFn = function() {
-        $('#main.lightbox .wrapper').css({ "height": "0", "width": "0", "background-color": "#fff"});
-    };
-
-    setUpLightbox('.horizontal-nav ul li', '#main', openCallbackFn, closeCallbackFn);
-
-
-    // Default WOW initialization
-    wow = new WOW({
-        boxClass: 'wow',
-        animateClass: 'animated',
-        offset: 0,
-        mobile: true,
-        live: true
-    });
-    wow.init();
-});
-
-//# sourceMappingURL=app.js.map
--- Full Width Hero/app-src/js/app.js
+++ Full Width Hero/app-src/js/app.js
@@ -1,5 +1,17 @@
+//@prepros-prepend _layout-framework.js
+
 $(document).ready(function() {
-    
+
+    var openCallbackFn = function() {
+        $('#main.lightbox .wrapper').css({ "height": "200px", "width": "200", "background-color": "#fff"});
+    };
+    var closeCallbackFn = function() {
+        $('#main.lightbox .wrapper').css({ "height": "0", "width": "0", "background-color": "#fff"});
+    };
+
+    setUpLightbox('.horizontal-nav ul li', '#main', openCallbackFn, closeCallbackFn);
+
+
     // Default WOW initialization
     wow = new WOW({
         boxClass: 'wow',
@@ -7,6 +19,6 @@ $(document).ready(function() {
         offset: 0,
         mobile: true,
         live: true
-    })
+    });
     wow.init();
 });
