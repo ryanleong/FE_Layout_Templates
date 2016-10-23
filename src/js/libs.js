34de36562cad7a2ac112599452030ab087fdc7b1
Ryan Leong <hello@ryanleong.net>
2016-10-23 17:00:05 +0800

    Added Rellax JS for parallax scrolling.

--- src/js/libs.js
+++ src/js/libs.js
@@ -6,3 +6,4 @@
 //@prepros-prepend ../../vendors/imagesloaded/imagesloaded.pkgd.min.js
 //@prepros-prepend ../../vendors/swiper/dist/js/swiper.jquery.js
 //@prepros-prepend ../../vendors/wow/dist/wow.min.js
+//@prepros-prepend ../../vendors/rellax/rellax.min.js
