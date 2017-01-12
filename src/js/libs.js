a7225c4f0f731cb585f06db948dce62416741497
Ryan Leong <hello@ryanleong.net>
2017-01-12 12:12:26 +0800

    Added retina js

--- src/js/libs.js
+++ src/js/libs.js
@@ -4,6 +4,7 @@
 
 //@prepros-prepend ../../vendors/jquery/dist/jquery.min.js
 //@prepros-prepend ../../vendors/imagesloaded/imagesloaded.pkgd.min.js
+//@prepros-prepend ../../vendors/retinajs/dist/retina.js
 //-@prepros-prepend ../../vendors/swiper/dist/js/swiper.jquery.js
 //@prepros-prepend ../../vendors/wow/dist/wow.min.js
 //-@prepros-prepend ../../vendors/rellax/rellax.min.js
